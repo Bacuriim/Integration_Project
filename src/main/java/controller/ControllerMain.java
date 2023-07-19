6ab693fb7da4e93f002139906efbc64bfb8b2fb9
Conrado Einstein <conradoeinsteinpro@gmail.com>
2023-07-19 13:08:44 -0300

    ## Backend update

--- src/main/java/controller/ControllerMain.java
+++ src/main/java/controller/ControllerMain.java
@@ -8,8 +8,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import model.CategoryEnum;
-import model.MeterLinesEnum;
-import model.MeterModelsEnum;
+import model.MeterLineEnum;
+import model.MeterModelEnum;
 
 public class ControllerMain implements Initializable {
 
@@ -17,7 +17,7 @@ public class ControllerMain implements Initializable {
     private TitledPane tpLine;
 
     @FXML
-    private ComboBox<MeterLinesEnum> cbbLine;
+    private ComboBox<MeterLineEnum> cbbLine;
 
     @FXML
     private TitledPane tpModel;
@@ -33,7 +33,7 @@ public class ControllerMain implements Initializable {
     public void initialize(URL location,ResourceBundle resources) {
         accordion.setExpandedPane(tpLine);
         tpModel.setDisable(true);
-        cbbLine.setItems(FXCollections.observableArrayList(MeterLinesEnum.values()));
+        cbbLine.setItems(FXCollections.observableArrayList(MeterLineEnum.values()));
         cbbLine.valueProperty().addListener(((observable, oldValue, newValue) -> openTreeView()));
     }
 
@@ -44,20 +44,20 @@ public class ControllerMain implements Initializable {
         switch(cbbLine.getValue().toString()){
 
             case "Ares":
-                TreeItem<String> rootFirst = new TreeItem<>(MeterLinesEnum.ARES.toString());
+                TreeItem<String> rootFirst = new TreeItem<>(MeterLineEnum.ARES.toString());
                 treeView.setRoot(rootFirst);
                 TreeItem<String> rootTB = new TreeItem<>(CategoryEnum.ARES_TB.toString());
                 TreeItem<String> rootTHS = new TreeItem<>(CategoryEnum.ARES_THS.toString());
 
                 rootFirst.getChildren().addAll(rootTB,rootTHS);
 
-                TreeItem<String> modelAres1 = new TreeItem<>(MeterModelsEnum.ARES_8023_15.toString());
-                TreeItem<String> modelAres2 = new TreeItem<>(MeterModelsEnum.ARES_8023_200.toString());
-                TreeItem<String> modelAres3 = new TreeItem<>(MeterModelsEnum.ARES_8023_25.toString());
+                TreeItem<String> modelAres1 = new TreeItem<>(MeterModelEnum.ARES_8023_15.toString());
+                TreeItem<String> modelAres2 = new TreeItem<>(MeterModelEnum.ARES_8023_200.toString());
+                TreeItem<String> modelAres3 = new TreeItem<>(MeterModelEnum.ARES_8023_25.toString());
 
-                TreeItem<String> modelAres4 = new TreeItem<>(MeterModelsEnum.ARES_7021.toString());
-                TreeItem<String> modelAres5 = new TreeItem<>(MeterModelsEnum.ARES_7031.toString());
-                TreeItem<String> modelAres6 = new TreeItem<>(MeterModelsEnum.ARES_7023.toString());
+                TreeItem<String> modelAres4 = new TreeItem<>(MeterModelEnum.ARES_7021.toString());
+                TreeItem<String> modelAres5 = new TreeItem<>(MeterModelEnum.ARES_7031.toString());
+                TreeItem<String> modelAres6 = new TreeItem<>(MeterModelEnum.ARES_7023.toString());
 
                 rootTHS.getChildren().add(modelAres1);
                 rootTHS.getChildren().add(modelAres2);
@@ -69,7 +69,7 @@ public class ControllerMain implements Initializable {
                 break;
 
             case "Cronos":
-                TreeItem<String> rootSecond = new TreeItem<>(MeterLinesEnum.CRONOS.toString());
+                TreeItem<String> rootSecond = new TreeItem<>(MeterLineEnum.CRONOS.toString());
                 treeView.setRoot(rootSecond);
                 TreeItem<String> rootCO = new TreeItem<>(CategoryEnum.CRONOS_OLD.toString());
                 TreeItem<String> rootCl = new TreeItem<>(CategoryEnum.CRONOS_L.toString());
@@ -77,19 +77,19 @@ public class ControllerMain implements Initializable {
 
                 rootSecond.getChildren().addAll(rootCO , rootCl , rootCNG);
 
-                TreeItem<String> modelCronos1 = new TreeItem<>(MeterModelsEnum.CRONOS_6001A.toString());
-                TreeItem<String> modelCronos2 = new TreeItem<>(MeterModelsEnum.CRONOS_6003.toString());
-                TreeItem<String> modelCronos3 = new TreeItem<>(MeterModelsEnum.CRONOS_7023.toString());
+                TreeItem<String> modelCronos1 = new TreeItem<>(MeterModelEnum.CRONOS_6001A.toString());
+                TreeItem<String> modelCronos2 = new TreeItem<>(MeterModelEnum.CRONOS_6003.toString());
+                TreeItem<String> modelCronos3 = new TreeItem<>(MeterModelEnum.CRONOS_7023.toString());
 
-                TreeItem<String> modelCronos4 = new TreeItem<>(MeterModelsEnum.CRONOS_6021L.toString());
-                TreeItem<String> modelCronos5 = new TreeItem<>(MeterModelsEnum.CRONOS_7023L.toString());
+                TreeItem<String> modelCronos4 = new TreeItem<>(MeterModelEnum.CRONOS_6021L.toString());
+                TreeItem<String> modelCronos5 = new TreeItem<>(MeterModelEnum.CRONOS_7023L.toString());
 
-                TreeItem<String> modelCronos6 = new TreeItem<>(MeterModelsEnum.CRONOS_6001NG.toString());
-                TreeItem<String> modelCronos7 = new TreeItem<>(MeterModelsEnum.CRONOS_6003NG.toString());
-                TreeItem<String> modelCronos8 = new TreeItem<>(MeterModelsEnum.CRONOS_6021NG.toString());
-                TreeItem<String> modelCronos9 = new TreeItem<>(MeterModelsEnum.CRONOS_6031NG.toString());
-                TreeItem<String> modelCronos10 = new TreeItem<>(MeterModelsEnum.CRONOS_7021NG.toString());
-                TreeItem<String> modelCronos11 = new TreeItem<>(MeterModelsEnum.CRONOS_7023NG.toString());
+                TreeItem<String> modelCronos6 = new TreeItem<>(MeterModelEnum.CRONOS_6001NG.toString());
+                TreeItem<String> modelCronos7 = new TreeItem<>(MeterModelEnum.CRONOS_6003NG.toString());
+                TreeItem<String> modelCronos8 = new TreeItem<>(MeterModelEnum.CRONOS_6021NG.toString());
+                TreeItem<String> modelCronos9 = new TreeItem<>(MeterModelEnum.CRONOS_6031NG.toString());
+                TreeItem<String> modelCronos10 = new TreeItem<>(MeterModelEnum.CRONOS_7021NG.toString());
+                TreeItem<String> modelCronos11 = new TreeItem<>(MeterModelEnum.CRONOS_7023NG.toString());
 
                 rootCO.getChildren().add(modelCronos1);
                 rootCO.getChildren().add(modelCronos2);
