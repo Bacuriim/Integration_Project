f595f8eff87dcd7016620159d0b8338872073c29
Conrado Einstein <conradoeinsteinpro@gmail.com>
2023-07-25 16:21:56 -0300

    ## bug fix

--- src/main/java/controller/ControllerMain.java
+++ src/main/java/controller/ControllerMain.java
@@ -5,9 +5,9 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import model.CategoryEntity;
-import model.LineEntity;
-import model.ModelEntity;
+import model.MeterCategoryEntity;
+import model.MeterLineEntity;
+import model.MeterModelEntity;
 import org.hibernate.Session;
 
 import java.net.URL;
@@ -20,13 +20,13 @@ public class ControllerMain implements Initializable {
     private TitledPane tpLine;
 
     @FXML
-    private ComboBox<LineEntity> cbbLine;
+    private ComboBox<MeterLineEntity> cbbLine;
 
     @FXML
     private TitledPane tpModel;
 
     @FXML
-    private TreeView<LineEntity> treeView;
+    private TreeView<MeterLineEntity> treeView;
 
     @FXML
     private Accordion accordion;
@@ -42,28 +42,28 @@ public class ControllerMain implements Initializable {
     }
 
     private void comboBoxSelect(){
-        List<LineEntity> lineList = session.createQuery("FROM LineEntity").list();
+        List<MeterLineEntity> lineList = session.createQuery("FROM MeterLineEntity").list();
 
         cbbLine.setItems(FXCollections.observableArrayList(lineList));
         cbbLine.valueProperty().addListener(((observable, oldValue, newValue) -> openTreeView(newValue)));
     }
 
-    private void openTreeView(LineEntity selectedLine) {
+    private void openTreeView(MeterLineEntity selectedLine) {
         tpLine.setExpanded(false);
         tpModel.setDisable(false);
         tpModel.setExpanded(true);
 
-        List<CategoryEntity> categoryEntityList = session.createQuery(String.format("FROM CategoryEntity WHERE id_line = '%s'",selectedLine)).list();
-        TreeItem setTreeView = new TreeItem<>(selectedLine);
-        setTreeView.setExpanded(true);
+        List<MeterCategoryEntity> meterCategoryEntityList = session.createQuery(String.format("FROM MeterCategoryEntity WHERE line_id = '%s'",selectedLine)).list();
+        TreeItem showTreeView = new TreeItem<>(selectedLine);
+        showTreeView.setExpanded(true);
 
-       categoryEntityList.forEach((category) -> {
-            TreeItem<CategoryEntity> categoryItem = new TreeItem<>(category);
-            setTreeView.getChildren().add(categoryItem);
+       meterCategoryEntityList.forEach((category) -> {
+            TreeItem<MeterCategoryEntity> categoryItem = new TreeItem<>(category);
+            showTreeView.getChildren().add(categoryItem);
 
-            List<ModelEntity> modelEntityList = session.createQuery(String.format("FROM ModelEntity WHERE id_category = '%s'",category)).list();
-            modelEntityList.forEach((model) -> categoryItem.getChildren().add(new TreeItem(model)));
+            List<MeterModelEntity> meterModelEntityList = session.createQuery(String.format("FROM MeterModelEntity WHERE category_id = '%s'",category)).list();
+            meterModelEntityList.forEach((model) -> categoryItem.getChildren().add(new TreeItem(model)));
        });
-       treeView.setRoot(setTreeView);
+       treeView.setRoot(showTreeView);
     }
 }
