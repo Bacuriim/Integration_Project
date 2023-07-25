bb5c73441f342cd8c7c18113f47d713267698d85
Einstein <conradoeinsteinpro@gmail.com>
2023-07-25 15:28:09 -0300

    ## Insertion of a database

--- src/main/java/controller/ControllerMain.java
+++ src/main/java/controller/ControllerMain.java
@@ -1,15 +1,18 @@
 package controller;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import ORM.MeterCategoryEntity;
+import ORM.MeterModelEntity;
+import connection.HibernateConnection;
+import ORM.MeterLineEntity;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import model.CategoryEnum;
-import model.MeterLineEnum;
-import model.MeterModelEnum;
+import org.hibernate.Session;
 
 public class ControllerMain implements Initializable {
 
@@ -17,95 +20,46 @@ public class ControllerMain implements Initializable {
     private TitledPane tpLine;
 
     @FXML
-    private ComboBox<MeterLineEnum> cbbLine;
+    private ComboBox<MeterLineEntity> cbbLine;
 
     @FXML
     private TitledPane tpModel;
 
     @FXML
-    private TreeView<String> treeView;
+    private TreeView<MeterLineEntity> treeView;
 
     @FXML
     private Accordion accordion;
 
-    @Override
+    public final Session session = HibernateConnection.buildSessionFactory().openSession();
 
+    @Override
     public void initialize(URL location,ResourceBundle resources) {
         accordion.setExpandedPane(tpLine);
         tpModel.setDisable(true);
-        cbbLine.setItems(FXCollections.observableArrayList(MeterLineEnum.values()));
-        cbbLine.valueProperty().addListener(((observable, oldValue, newValue) -> openTreeView()));
+        openComboBox();
     }
-
-    private void openTreeView() {
+    private void openComboBox(){
+        List<MeterLineEntity> lineList = session.createQuery("FROM MeterLineEntity").list();
+        cbbLine.setItems(FXCollections.observableArrayList(lineList));
+        cbbLine.valueProperty().addListener(((observable, oldValue, newValue) -> openTreeView(newValue)));
+    }
+    private void openTreeView(MeterLineEntity selectedLine) {
         tpLine.setExpanded(false);
         tpModel.setDisable(false);
         tpModel.setExpanded(true);
 
-        switch(cbbLine.getValue().toString()){
+        List<MeterCategoryEntity> categoryList = session.createQuery(String.format("FROM MeterCategoryEntity WHERE line_id = '%s'",selectedLine)).list();
+        TreeItem showTreeView = new TreeItem<>(selectedLine);
+        showTreeView.setExpanded(true);
 
-            case "Ares":
-                TreeItem<String> rootFirst = new TreeItem<>(MeterLineEnum.ARES.toString());
-                treeView.setRoot(rootFirst);
-                TreeItem<String> rootTB = new TreeItem<>(CategoryEnum.ARES_TB.toString());
-                TreeItem<String> rootTHS = new TreeItem<>(CategoryEnum.ARES_THS.toString());
+        categoryList.forEach((category) -> {
+            TreeItem<MeterCategoryEntity> categoryItem = new TreeItem<>(category);
+            showTreeView.getChildren().add(categoryItem);
 
-                rootFirst.getChildren().addAll(rootTB,rootTHS);
-
-                TreeItem<String> modelAres1 = new TreeItem<>(MeterModelEnum.ARES_8023_15.toString());
-                TreeItem<String> modelAres2 = new TreeItem<>(MeterModelEnum.ARES_8023_200.toString());
-                TreeItem<String> modelAres3 = new TreeItem<>(MeterModelEnum.ARES_8023_25.toString());
-
-                TreeItem<String> modelAres4 = new TreeItem<>(MeterModelEnum.ARES_7021.toString());
-                TreeItem<String> modelAres5 = new TreeItem<>(MeterModelEnum.ARES_7031.toString());
-                TreeItem<String> modelAres6 = new TreeItem<>(MeterModelEnum.ARES_7023.toString());
-
-                rootTHS.getChildren().add(modelAres1);
-                rootTHS.getChildren().add(modelAres2);
-                rootTHS.getChildren().add(modelAres3);
-
-                rootTB.getChildren().add(modelAres4);
-                rootTB.getChildren().add(modelAres5);
-                rootTB.getChildren().add(modelAres6);
-                break;
-
-            case "Cronos":
-                TreeItem<String> rootSecond = new TreeItem<>(MeterLineEnum.CRONOS.toString());
-                treeView.setRoot(rootSecond);
-                TreeItem<String> rootCO = new TreeItem<>(CategoryEnum.CRONOS_OLD.toString());
-                TreeItem<String> rootCl = new TreeItem<>(CategoryEnum.CRONOS_L.toString());
-                TreeItem<String> rootCNG = new TreeItem<>(CategoryEnum.CRONOS_NG.toString());
-
-                rootSecond.getChildren().addAll(rootCO , rootCl , rootCNG);
-
-                TreeItem<String> modelCronos1 = new TreeItem<>(MeterModelEnum.CRONOS_6001A.toString());
-                TreeItem<String> modelCronos2 = new TreeItem<>(MeterModelEnum.CRONOS_6003.toString());
-                TreeItem<String> modelCronos3 = new TreeItem<>(MeterModelEnum.CRONOS_7023.toString());
-
-                TreeItem<String> modelCronos4 = new TreeItem<>(MeterModelEnum.CRONOS_6021L.toString());
-                TreeItem<String> modelCronos5 = new TreeItem<>(MeterModelEnum.CRONOS_7023L.toString());
-
-                TreeItem<String> modelCronos6 = new TreeItem<>(MeterModelEnum.CRONOS_6001NG.toString());
-                TreeItem<String> modelCronos7 = new TreeItem<>(MeterModelEnum.CRONOS_6003NG.toString());
-                TreeItem<String> modelCronos8 = new TreeItem<>(MeterModelEnum.CRONOS_6021NG.toString());
-                TreeItem<String> modelCronos9 = new TreeItem<>(MeterModelEnum.CRONOS_6031NG.toString());
-                TreeItem<String> modelCronos10 = new TreeItem<>(MeterModelEnum.CRONOS_7021NG.toString());
-                TreeItem<String> modelCronos11 = new TreeItem<>(MeterModelEnum.CRONOS_7023NG.toString());
-
-                rootCO.getChildren().add(modelCronos1);
-                rootCO.getChildren().add(modelCronos2);
-                rootCO.getChildren().add(modelCronos3);
-
-                rootCl.getChildren().add(modelCronos4);
-                rootCl.getChildren().add(modelCronos5);
-
-                rootCNG.getChildren().add(modelCronos6);
-                rootCNG.getChildren().add(modelCronos7);
-                rootCNG.getChildren().add(modelCronos8);
-                rootCNG.getChildren().add(modelCronos9);
-                rootCNG.getChildren().add(modelCronos10);
-                rootCNG.getChildren().add(modelCronos11);
-                break;
-        }
+            List<MeterModelEntity> modelList = session.createQuery(String.format("FROM MeterModelEntity WHERE category_id = '%s'",category)).list();
+            modelList.forEach((model) -> categoryItem.getChildren().add(new TreeItem(model)));
+        });
+        treeView.setRoot(showTreeView);
     }
 }
