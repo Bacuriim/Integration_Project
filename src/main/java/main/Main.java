7f5063abf5759053a198160bbf08876b563db930
Conrado Einstein <conradoeinsteinpro@gmail.com>
2023-07-25 15:52:13 -0300

    ## Insertion of a database

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Integration.fxml"));
             Scene cena = new Scene(loader.load());
